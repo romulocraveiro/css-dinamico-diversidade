d7d60e1aa11eb9005dd40fabd25cc77f46bb4b91
Romulo Craveiro <romulocraveiro@gmail.com>
2022-06-25 14:37:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# CSS dinâmico: viva a diversidade!
+
+## Objetivos / Aims
+
+### 1) Usar `setProperty()` e criação de variáveis para mudar a cor do background da página dinamicamente e mostrar um vídeo diferente para cada cor;
+
+### _Use `setProperty()` to create variables to change the pages's background color and show a video:_
+
+###  2) Mostrar um modal com a definição de cada letra da sigla LGBTQIAP+ e, ao clicar em qualquer lugar da tela, fazer o modal desaparecer e mostrar o conteúdo desejado.
+###  _Show a modal that displays a definition for each letter of the acronym LGBTQIAP+ and, by clicking anywhere on the screen, make the modal disappear and to show the desired content._
+
+## Problemas que consegui resolver / Problems I was able to solve
+
+* Tirar a borda default do iframe e substituí-la por uma de neon.
+* _Remove the default iframe border and substitute it for a neon one._
+
+* Estilizar cores dos backgrounds adicionando "ba" aos rgba de modo que não ficassem tão vibrantes e cansassem a visão.
+* _Styling the background colors by adding "ba" to the rgba so that they weren't so vibrating and tiring for the eye sight._
+
+* Eu quis que minha bandeira tremulasse numa animação com loop.Daí eu encontrei um site bem legal que faz isso automaticamente (ver a sessão de links); 
+
+*_I wanted my rainbow flag to have an animation loop that would make it wave. So I found this cool website that makes it for you (see links below)._
+
+## Algumas lições que aprendi / Some lessons I learned
+
+* Se você digitar as iniciais de uma propriedade ou método no JavaScript, um menu aparece com opções, ou ao se clicar "Enter" você muito provavelmente terá selecionado o que queria; 
+* _If you type the initials of a property or method on JavaScript, a menu will be displayed with options, or when you click "Enter" key it is very likely you will have selected what you wanted;_
+
+* Fazer com que um vídeo do tipo "shorts" do youtube pudesse funcionar na página.
+* _Making a Youtube short work on a page._
+
+## Tecnologias / Technologies
+
+* CSS
+* HTML
+* JavaScript
+
+## Links 
+
+* [Cubos Academy](https://cubos.academy/)
+* [How to make a neon border - Stack Overflow](https://stackoverflow.com/questions/36478969/how-to-make-a-dotted-neon-border)
+* [Tutorial para inserir vídeos do Youtube - Embeded Youtube videos tutorial](https://www.youtube.com/watch?v=xd24_NnzKy4)
+* [Onde montei o gif da bandeira - Here is where I costumized my flag gif:](https://makesweet.com/)
+* [Efeito piscar do neon do H1 - H1's flickering neon effect:](https://www.instagram.com/p/Ce2mqa3pttD/)
+* [Bandeirinha que vai na aba do navegador / Flag icon that goes in browser's tab:](https://www.flaticon.com/free-icons/lgtbi)
+* [Criar e adicionar um favicon para a aba do navegador: Creating and adding a favicon to browser's tab:](https://www.metatags.org/seo-tips/design-tips/favicon-ico/)
+* [Converter um shorts do Youtube em um embeded usando aba anônima / Convert a Youtube shorts into an embeded one with incognito tab:](https://linustechtips.com/topic/1360774-embedding-of-youtube-shorts/)
+
+## Autor / Author
+
+| Foto                                         | Nome                               | GitHub                                               | Likedin                                                 | E-mail                   |
+| -------------------------------------------- | ---------------------------------- | ---------------------------------------------------- | ------------------------------------------------------- | ------------------------ |
+| <img src="./assets/github.jpeg" width="100px"> | Romulo Craveiro de Sousa Tartaruga | [Romulo Craveiro](https://github.com/romulocraveiro) | [Linkedin](https://www.linkedin.com/in/romulocraveiro/) | romulocraveiro@gmail.com |
---
+++ script.js
@@ -0,0 +1,126 @@
+const body = document.querySelector('body');
+const rainbowColors = document.querySelectorAll('.rainbow-color');
+const img = document.querySelector('img');
+const h1 = document.querySelector('h1');
+const footer = document.querySelector('footer'); 
+const purple = document.querySelector('.purple')
+const modal = document.querySelector('.modal');
+const title = document.querySelector('h2');
+const paragraph = document.querySelector('p');
+const video = document.querySelector('.video');
+const iFrame = document.createElement('iframe');
+const a = document.querySelector('a');
+const topPurpleVideo = document.createElement('iframe');
+const photoBy = document.querySelector('.photo-by');
+
+function handleChangeBackgroundColor(e){
+    video.append(iFrame);
+    
+    if(e.target.id === 'purple'){
+        topPurpleVideo.setAttribute('src', 'https://www.youtube.com/embed/XeuZG0zyi0k')
+        iFrame.setAttribute('src', 'https://www.youtube.com/embed/lwzBfbw9l-w');
+        iFrame.style.width = '360px';
+        iFrame.style.height = '275px';
+        body.style.backgroundImage = 'url(./assets/rochelle-brown-P-K0CNvhBoI-unsplash.jpg)';
+        body.style.backgroundSize = 'cover';
+        title.innerText = 'Cores e o sinal de + da sigla LGBTQIAP+';
+        paragraph.innerText = 'Ikaro Kadoshi explica o significado das cores da bandeira LGBTQIAP+, e Rita Von Hunty fala sobre o sinal de +';
+        modal.style.display = 'flex';
+        modal.style.backgroundColor = '';
+        video.classList.add('purple-video');
+        topPurpleVideo.classList.add('purple-top-video');
+        video.append(topPurpleVideo);
+        topPurpleVideo.style.display = 'flex';
+        h1.innerText = '';
+        iFrame.style.marginBottom = 0;
+        footer.style.display = 'none';
+
+    } else {
+        h1.innerText = '';
+        img.style.display = 'none';
+        body.style.backgroundImage = '';
+        body.style.setProperty('--primary-color', `${e.target.id}`);
+        video.classList.remove('purple-video');
+        modal.style.display ='flex';
+        topPurpleVideo.style.display = 'none';
+        iFrame.style.width = '560px';
+        iFrame.style.height = '315px';
+        a.style.display = 'none';
+        a.setAttribute('target', '_blank');
+        photoBy.style.display = 'none';
+        footer.style.display = 'flex';
+
+        
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = '#e50303ba';
+            iFrame.setAttribute('src', 'https://www.youtube.com/embed/0ZQqYR3HoPM');
+            title.innerText = 'Lésbicas';
+            paragraph.innerText = 'Mulheres que se sentem atraídas emocionalmente e/ou fisicamente por outras mulheres, sejam elas cisgênero ou transgênero.';
+
+            }else if(e.target.id === 'orangered'){
+                body.style.backgroundColor = '#ff4400cb';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/9DHROmg0Mek');
+                title.innerText = 'Gays';
+                paragraph.innerText = 'Homens que se sentem atraídos emocionalmente e/ou fisicamente por outros homens, sejam cisgênero ou transgênero.';
+            }else if(e.target.id === 'yellow'){
+                body.style.backgroundColor = '#ffff00ba';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/RuFY9eA67OY');
+                title.innerText = 'Bissexuais';
+                paragraph.innerText = 'Pessoas que se sentem atraídas emocionalmente e/ou fisicamente por homens e mulheres.';
+            }else if(e.target.id === 'green'){
+                body.style.backgroundColor = '#008000';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/j57S7e8wSB0');
+                title.innerText = 'Transgêneros';
+                paragraph.innerText = 'Pessoas cujo gênero ou expressão de gênero difere daquela determinada no nascimento. Podem ser héteros, lésbicas, gays, bissexuais ou queer.';
+            }else if(e.target.id === 'blue'){
+                body.style.backgroundColor = '#0000ffe3';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/a7Ai_UFyVdg')
+                title.innerText = 'Queer';
+                paragraph.innerText = 'Antes pejorativo na língua inglesa, agora é um termo mais abrangente, usado por pessoas que não se identificam com as normas de heterossexualidade ou gênero binário.';
+            }else if(e.target.id === 'cornflowerblue'){
+                body.style.backgroundColor = '#6495ed';
+                title.innerText = 'Intersexo';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/6l2GDA1qnSo');
+                paragraph.innerText = 'Pessoas cujo sexo determinado no nascimento não se encaixa no que é socialmente considerado como masculino ou feminino porque sua genitália, nível de produção de hormônio e/ou composição cromossômica são ambíguos ou não binários.';
+            }else if(e.target.id === 'pink'){
+                body.style.backgroundColor = '#ffc0cb';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/ajBvfyrOv28');
+                title.innerText = 'Assexuais';
+                paragraph.innerText = 'Pessoas que não sentem atração sexual, podendo ainda desenvolver o amor e o afeto.';
+            }else if(e.target.id === 'violet'){
+                body.style.backgroundColor = '#ee82ee';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/Tzg_QvOkcDw');
+                title.innerText = 'Pansexuais';
+                paragraph.innerText = 'Pessoas cujo amor e atração física se direcionam a outras independente de sua identidade de gênero ou sexo biológico.';
+            }else if(e.target.id === 'black'){
+                body.style.backgroundColor = '#000000';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/Z6acPxenN14');
+                title.innerText = 'Movimento Negro x LGBTQIAP+';
+                paragraph.innerText = 'Neste vídeo, Alê Garcia comenta as intersecções entre os movimentos negro e LGBTQIAP+, sob uma perspectiva histórica que aborda os contextos brasileiro e estadunidense.';
+                a.style.display = 'flex';
+                a.setAttribute('href', 'https://www.geledes.org.br/atriz-de-pose-que-aprendeu-a-programar-sozinha-vem-ao-brasil-divulgar-startup-de-ti-que-criou-para-ajudar-pessoas-trans-como-ela/');
+            }else if(e.target.id === 'brown'){
+                body.style.backgroundColor = '#a52a2a';
+                iFrame.setAttribute('src', 'https://www.youtube.com/embed/QsPTcCRmKFg');
+                title.innerText = 'Angelica Ross';
+                paragraph.innerText = 'Neste TED Talk inspirador, a atriz e criadora da startup de T.I. "Trans Tech Social" relata sua história de superação e afirma categoricamente: "Eu tive a audácia de pensar que os sonhos de uma mulher preta trans importam também."';
+                a.style.display = 'flex';
+                a.innerText = 'Clique aqui e saiba mais sobre Angelica Ross.'
+                a.setAttribute('href', 'https://www.geledes.org.br/atriz-de-pose-que-aprendeu-a-programar-sozinha-vem-ao-brasil-divulgar-startup-de-ti-que-criou-para-ajudar-pessoas-trans-como-ela/');
+                a.setAttribute('target', '_blank'); 
+            }
+    }
+}
+    
+modal.addEventListener('click', function (){
+    modal.style.display = 'none';
+})
+
+for (const rainbowColor of rainbowColors) {
+    rainbowColor.addEventListener('click', handleChangeBackgroundColor );
+}
+    
+// PRÓXIMO DESAFIO:
+// Passar para o REACT e usar Material UI - biblioteca de componentes react, assim como a Chakra UI (testar ele tb depois)
+// Componentes: video, botoes
+// Fazer modal com card: props - título e texto
